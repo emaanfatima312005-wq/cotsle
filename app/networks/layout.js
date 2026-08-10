aee7b8413f2fc86c41e34e90a979d74c7b1b8b59
Tayyaba Sadaqat <arishtayb818@gmail.com>
2026-08-10 17:27:09 +0500

    Networks AI implemented on frontend

--- app/networks/layout.js
+++ app/networks/layout.js
@@ -1,11 +1,13 @@
 import Navbar from "@/components/networks/Navbar";
 import Footer from "@/components/networks/Footer";
+import NetworkAIAssistant from "@/components/networks/NetworkAIAssistant";
 
 export default function NetworksLayout({ children }) {
   return (
     <>
       <Navbar />
       {children}
+      <NetworkAIAssistant />
       <Footer />
     </>
   );
